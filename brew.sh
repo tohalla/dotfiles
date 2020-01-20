c313ab1725757751c70ab11ba82d9bc16dcbc3b3
Mathias Bynens <mathias@qiwi.be>
2020-01-20 07:54:41 +0100

    .gitconfig: Add `g whoami`
    https://twitter.com/ericlaw/status/1218759854002724864

--- brew.sh
+++ brew.sh
@@ -95,6 +95,10 @@ brew install tree
 brew install vbindiff
 
 brew install --HEAD neovim
+brew install the_silver_searcher
+brew install --HEAD universal-ctags/universal-ctags/universal-ctags
+brew install pinentry-mac
+brew install gpg2
 
 # Remove outdated versions from the cellar.
 brew cleanup
